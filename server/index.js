07b7b667806deea2b3c44dadfabc4dfd4c592e7a
Harlan Heath <HarlanDHeath@gmail.com>
2018-07-06 12:02:19 -0500

    update 4

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance);
+});
+
 app.listen(port, () => {
   console.log(`I am listening on port ${port}`);
 });
--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,7 +1,61 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imageurl: " ",
+      prodName: " ",
+      price: 0
+    };
+  }
+
+  resetState() {
+    this.setState({
+      imageurl: " ",
+      prodName: " ",
+      price: 0
+    });
+  }
+
+  urlInput(val) {
+    this.setState({
+      imageurl: val
+    });
+  }
+
+  nameInput(val) {
+    this.setState({
+      prodName: val
+    });
+  }
+
+  priceInput(val) {
+    this.setState({
+      price: val
+    });
+  }
+
   render() {
-    return <div> Form </div>;
+    console.log(this.state);
+    return (
+      <div>
+        <div>Form</div>
+        <input
+          placeholder="Image URL"
+          onChange={e => this.urlInput(e.target.value)}
+        />
+        <input
+          placeholder="Product Name"
+          onChange={e => this.nameInput(e.target.value)}
+        />
+        <input
+          placeHolder="Price"
+          onChange={e => this.priceInput(e.target.value)}
+        />
+        <button>Add to Inventory</button>
+        <button onClick={e => this.resetState()}>Cancel</button>
+      </div>
+    );
   }
 }
